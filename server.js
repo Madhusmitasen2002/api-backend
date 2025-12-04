606076bd52bd84911688440c09f700a0dc2e48b1
Madhusmita Sen <madhusmitasen27@gmail.com>
2025-12-04 14:12:41 +0530

    fixes

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "Backend running successfully!" });
+  res.send("Backend is running!");
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
